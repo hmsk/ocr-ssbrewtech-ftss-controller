f8e05a6a25318856b8e6eac2a4482dc58e6b2f33
hmsk <k.hamasaki@gmail.com>
2020-09-05 00:17:52 -0700

    Take hour and second for timestamp, adjust offset

--- bin/process.rb
+++ bin/process.rb
@@ -6,14 +6,15 @@ class BrewtechOCRRunner
   ROTATION = '90'
 
   def initialize(path = Dir.pwd)
-    @path_for_captured_image = "#{path}/#{Time.now.strftime('%y%m%d')}_capture.jpg"
-    @path_for_cropped_image = "#{path}/#{Time.now.strftime('%y%m%d')}_crop.jpg"
+    timestamp = Time.now.strftime('%y%m%d%H%M%S')
+    @path_for_captured_image = "#{path}/#{timestamp}_capture.jpg"
+    @path_for_cropped_image = "#{path}/#{timestamp}_crop.jpg"
   end
 
   def capture
     `raspistill -gs square -o #{@path_for_captured_image} -sh #{SHARPNESS} -ISO #{ISO} -rot #{ROTATION}`
     image = MiniMagick::Image.open(@path_for_captured_image)
-    image.crop '1156x598+425+720'
+    image.crop '1156x598+425+700'
     image.write @path_for_cropped_image
   end
 
