74a76add98081c46084d0b933d53dcb09286b076
hmsk <k.hamasaki@gmail.com>
2020-09-04 23:48:50 -0700

    Put cropping between capturing and ocr-ing

--- bin/process.rb
+++ bin/process.rb
@@ -5,14 +5,19 @@ class BrewtechOCRRunner
 
   def initialize(path = Dir.pwd)
     @path_for_captured_image = "#{path}/#{Time.now.strftime('%y%m%d')}_capture.jpg"
+    @path_for_cropped_image = "#{path}/#{Time.now.strftime('%y%m%d')}_crop.jpg"
   end
 
   def capture
     `raspistill -gs square -o #{@path_for_captured_image} -sh #{SHARPNESS} -ISO #{ISO} -rot #{ROTATION}`
+    image = MiniMagick::Image.open(@path_for_captured_image)
+    image.crop '1156x598+425+730'
+    image.write @path_for_cropped_image
   end
 
   def report_temperature
-    temperature = `ssocr --foreground=white --background=black -d -1 #{@path_for_captured_image} -t 80`
+    temperature = `ssocr --foreground=white --background=black -d -1 #{@path_for_cropped_image} -t 80`
+    p temperature
   end
 end
 
