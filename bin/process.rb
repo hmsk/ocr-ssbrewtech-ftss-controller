cf27924ac9be938dd41ce6c9f669a1bfdc232225
hmsk <k.hamasaki@gmail.com>
2020-09-07 00:17:23 -0700

    Use config.yml to adjust parameters for handling an image

--- bin/process.rb
+++ bin/process.rb
@@ -1,25 +1,26 @@
 Bundler.require(:default)
 
 class BrewtechOCRRunner
-  ISO = '64'
-  SHARPNESS = '100'
-  ROTATION = '90'
-
   def initialize(path = Dir.pwd)
     timestamp = Time.now.strftime('%y%m%d%H%M%S')
     @path_for_captured_image = "#{path}/#{timestamp}_capture.jpg"
     @path_for_cropped_image = "#{path}/#{timestamp}_crop.jpg"
+
+    @config = YAML.load_file("#{Dir.pwd}/config.yml")
   end
 
   def capture
-    `raspistill -gs square -o #{@path_for_captured_image} -sh #{SHARPNESS} -ISO #{ISO} -rot #{ROTATION}`
+    camera_conf = @config['camera']
+    `raspistill -gs square -o #{@path_for_captured_image} -sh #{camera_conf['sharpness']} -ISO #{camera_conf['iso']} -rot #{camera_conf['rotation']}`
+
+    crop = @config['crop']
     image = MiniMagick::Image.open(@path_for_captured_image)
-    image.crop '1170x598+406+680'
+    image.crop "#{crop['width']}x#{crop['height']}+#{crop['x']}+#{crop['y']}"
     image.write @path_for_cropped_image
   end
 
   def report_temperature
-    temperature = `ssocr --foreground=white --background=black -d 3 #{@path_for_cropped_image} -t 95`.chomp.gsub('.', '')
+    temperature = `ssocr --foreground=white --background=black -d 3 #{@path_for_cropped_image} -t #{@config['ocr']['threshold']}`.chomp.gsub('.', '')
     puts "temp: " + temperature.chars.insert(2, '.').join('') + " F"
   end
 end
